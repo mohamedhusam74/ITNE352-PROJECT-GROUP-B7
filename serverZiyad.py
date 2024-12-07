edfd00c20897d601d372828ce22e47f0b20642de
ziyad131 <Hacky.2131@gmail.com>
2024-12-07 20:52:14 +0300

    Update serverZiyad.py

--- serverZiyad.py
+++ serverZiyad.py
@@ -8,3 +8,26 @@ HOST = '127.0.0.1'
 PORT = 34142  
 API_KEY = "ea46e59a1e754401b72c3cb895e374e7"  
 GROUP_ID = "B7"  
+
+
+#Processing headlines
+def p_headlines(news_info):
+    """
+    Parse and extract headlines from the NewsAPI response.
+    :param news_info: Dictionary containing news articles
+    :return: List of dictionaries with parsed article details
+    """
+    articles = []
+    for article in news_info.get('articles', []):
+        articles.append({
+            'source_name': article.get('source', {}).get('name'),
+            'author': article.get('author'),
+            'title': article.get('title'),
+            'url': article.get('url'),
+            'description': article.get('description'),
+            'publish_date': article.get('publishedAt', '')[:10],  
+            'publish_time': article.get('publishedAt', '')[11:19],  
+        })
+    return articles
+
+
