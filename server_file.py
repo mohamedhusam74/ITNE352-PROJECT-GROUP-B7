cdc1fd6c25c161a9aae4c44af2784f56ae24ce7c
ziyad131 <Hacky.2131@gmail.com>
2024-12-11 09:47:53 +0300

    Update server_file.py

--- server_file.py
+++ server_file.py
@@ -39,3 +39,23 @@ def handler(clientSoc,clientName):
 
         news_data = response.json()  
         file_name = f"{clientName}_{data_type}_{GROUP_ID}.json" 
+
+         # Save the raw response to a file
+        with open(file_name, 'w') as json_file:
+            json.dump(news_data, json_file, indent=4)
+
+        # Prepare response payload based on the data type requested
+        if data_type == "headlines":
+            response_payload = {
+                'type': 'headlines',
+                'data': extractors.extract_headlines(news_data)[:15]  
+            }
+        elif data_type == "sources":
+            response_payload = {
+                'type': 'sources',
+                'data': extractors.extract_sources(news_data)[:15] 
+            }
+        else:
+            response_payload = {'error': 'wrong data type requested'}
+
+        clientSoc.send(json.dumps(response_payload).encode()) 
